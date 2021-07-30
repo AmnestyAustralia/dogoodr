d2f6ae934183c6581820c0a187a5f2fac12f2396
markbrisbane <mark@improvesilence.com>
2021-07-30 09:50:31 +0800

    Documented convenience functions for since argument, added ever function

--- R/actions.R
+++ R/actions.R
@@ -2,6 +2,8 @@
 #'
 #' Returns a feed the latest action log entries for the entire account. It includes the campaign, action, and form data submitted by the supporter.
 #'
+#' Useful convenience functions for 'since' argument include ever() which returns the first second of the CE, this_morning() which returns the first second of today first_moment() which returns the first second of any day, yesterday(), days_ago(), weeks_ago() which behave similarly.
+#'
 #' @param since time object representing the earliest action to be returned
 #' @param campaign_id vector of campaign id integers to be returned
 #' @param starting_with should the return list be arranged to start with the earliest or latest action
@@ -42,8 +44,22 @@ parse_starting_with <- function(selected_arg){
   }
 }
 
-first_moment <- function(day) day + lubridate::seconds(1)
-this_morning <- function() first_moment(lubridate::today())
-days_ago <- function(n) first_moment(lubridate::today()-lubridate::days(n))
-yesterday <- function() days_ago(1)
-weeks_ago <- function(n) first_moment(lubridate::today()-lubridate::weeks(n))
+#' Convenience functions dg_actions "since" argument
+#'
+#' @param day a date value
+#' @param n  an integer for the number of days or weeks previous to this moment
+#'
+#' @return a datetime object
+#' @export
+#' @importFrom lubridate seconds today days weeks ymd_hms
+first_moment <- function(day){ day + lubridate::seconds(1) }
+#' @describeIn first_moment returns first moment of the current day
+this_morning <- function(){ first_moment(lubridate::today()) }
+#' @describeIn first_moment returns first moment n days ago
+days_ago <- function(n){ first_moment(lubridate::today()-lubridate::days(n)) }
+#' @describeIn first_moment returns first moment yesterday
+yesterday <- function(){ days_ago(1) }
+#' @describeIn first_moment returns first moment n weeks ago
+weeks_ago <- function(n){ first_moment(lubridate::today()-lubridate::weeks(n)) }
+#' @describeIn first_moment returns first moment of the common era
+ever <- function(){ lubridate::ymd_hms("0001-01-01T00:00:01") }
